072f9ac2f44c80014e8603374f87e88ed0bae7bd
rio <yakimenko.kir@gmail.com>
2023-12-23 18:16:03 +0100

    Have to debug it

--- src/MatrixGen.hs
+++ src/MatrixGen.hs
@@ -1,23 +1,36 @@
-module MatrixGen(generateBs, generateLs)where
+module MatrixGen(generateBs, generateLs, coefficients)where
 
 import BaseFunctions
 import Spec
 import Math.GaussianQuadratureIntegration(nIntegrate256)
+import Data.Maybe (fromJust)
+import Numeric.LinearAlgebra (flatten, fromLists, linearSolve, luSolve, toList, toLists, (><))
+
+-- approxU :: Int -> Double -> Double
+-- approxU n x =
+--   sum . zipWith (\i c_i -> c_i * basisF i x) [0 ..] . coefficients $ n
+--   where
+--     basisF = eI
+
+coefficients :: [Double]
+coefficients = 0 : (toList . flatten $ fromJust (linearSolve b_matrix l_vector))
+  where
+    b_matrix = fromLists generateBs 
+    l_vector = (n >< 1) generateLs
 
 n :: Int
 n = defaultN
 
-generateLs :: Int -> [Double]
-generateLs n = [generateL i | i <- [0..n]]
+generateLs :: [Double]
+generateLs = [generateL i | i <- [0..n]]
 
 generateL :: Int -> Double
 generateL i 
     |i  == 0 = 20
     |otherwise = 0
 
-
-generateBs :: Int -> [[Double]]
-generateBs n = [ [ generateB i j | i <- [1..n] ] | j <- [1..n] ]
+generateBs :: [[Double]]
+generateBs = [ [ generateB i j | i <- [1..n] ] | j <- [1..n] ]
 
 generateB :: Int -> Int -> Double
 generateB i j  
